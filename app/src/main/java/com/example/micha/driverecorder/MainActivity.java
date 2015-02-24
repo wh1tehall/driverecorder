74e44f4253933e5f4918422caafd6dedc2493852
wh1tehall <whitehall@o2.pl>
2015-02-24 21:52:37 +0100

    Added average error calculation
    Application now calculates average acceleration and average error, also
    displays current acceleration as a horizontal bar.

--- app/src/main/java/com/example/micha/driverecorder/MainActivity.java
+++ app/src/main/java/com/example/micha/driverecorder/MainActivity.java
@@ -12,28 +12,52 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import java.util.EventListener;
 
 
-public class MainActivity extends ActionBarActivity implements SensorEventListener, Runnable {
+public class MainActivity extends ActionBarActivity implements SensorEventListener, EventListener,Runnable {
 
     private Handler customHandler = new Handler();
     private TextView timerValue;
+    private TextView average;
+    private TextView average_error;
+    private ProgressBar pBar;
+    private Button button;
     private Sensor acc;
     private double reading;
+    private double avgerror;
+    private double avg;
     Thread worker;
     boolean work = true;
+    boolean start= false;
+    boolean firstMeasurment=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         timerValue = (TextView)findViewById(R.id.textView1);
+        average = (TextView)findViewById(R.id.textView);
+        average_error = (TextView)findViewById(R.id.textView2);
+        pBar = (ProgressBar)findViewById(R.id.progressBar);
+        pBar.setMax(100);
+
+        button =(Button)findViewById(R.id.button);
+
         Log.w("D:","BBB");
         acc = getSensor();
 
         Log.w("C:","AAA");
         worker = new Thread(this);
+    }
+
+    public void startMeasure(View view){
+        start= !start;
+        firstMeasurment=true;
     }
 
     @Override
@@ -111,6 +135,21 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
             h.post(new Runnable(){
                 @Override public void run(){
                     timerValue.setText(String.format("%f",  reading));
+                    pBar.setProgress((int)(100*(reading/50)));
+                    if (start){
+                        if (!firstMeasurment){
+
+                            avgerror=(Math.abs(avg-reading)+avgerror)/2;
+                            avg=(avg+reading)/2;
+                        }
+                        else{
+                            firstMeasurment=false;
+                            avg=reading;
+                            avgerror=0;
+                        }
+                    }
+                    average.setText(String.format("%f", avg));
+                    average_error.setText(String.format("%f",avgerror));
 
                 }
                 });
